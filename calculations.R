d8ddec7bc81e061d5447eb14503ea8c67afb8080
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-12-05 20:17:04 +0100

    first notes and calculations

---
+++ calculations.R
@@ -0,0 +1,76 @@
+rm(list = ls()); gc(); gc()
+# options(java.parameters = "-Xmx4096m")#8192
+# options(java.parameters = "-XX:-UseConcMarkSweepGC")
+# options(java.parameters = "-XX:-UseGCOverheadLimit")
+options(bitmapType='cairo')
+options(scipen = 999)
+#library(bbbi)
+library(ggplot2)
+#library(RODBC)
+library(dplyr)
+#library(tidyr)
+#library(data.table)
+
+# Define your workspace: "X:/xxx/"
+wd <- "D:/github/statistics101/"
+setwd(wd)
+
+
+set.seed(42)
+
+## colour-blind palettes: 
+# The palette with grey:
+cbgPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# The palette with black:
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# x values for plotting
+x <- seq(-5, 5, by = 0.01)
+
+
+ggplot()+
+  # poisson distribution
+  geom_point(
+    aes(x=unique(as.integer(x)), y=dpois(unique(as.integer(x)), 2)), size = 3.5, colour = cbgPalette[2]
+  )+
+  # exponential distribution
+  geom_line(
+    aes(x=x[x>0], y=dexp(x[x>0], 0.5)), size = 1.5, colour = cbgPalette[3]
+  )+
+  # uniform distribution
+  geom_line(
+    aes(x=x, y=dunif(x, -4, 4)), size = 1.5, colour = cbgPalette[4]
+  )+
+  # normal distribution
+  geom_line(
+    aes(x=x, y=dnorm(x)), size = 1.5, colour = cbgPalette[6]
+  )+
+#   # lognormal distribution
+#   geom_line(
+#     aes(x=x, y=dlnorm(x)), size = 1.5, colour = cbgPalette[6]
+#   )+
+  labs(x = "", y = "", title = "Statistical distributions")
+  
+
+set.seed(1001);poisson <- rpois(50, 5)+10
+hist(poisson)
+
+ggplot()+
+  geom_point(aes(x=poisson, y=1), position = position_jitter(w = 0.01, h = 0.05))
+
+set.seed(1001);exp <- rexp(50, 5)+10
+hist(exp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ notes.md
@@ -0,0 +1,29 @@
+http://datascienceplus.com/standard-deviation-vs-standard-error/
+
+## Overview
+
+- random variables
+    - uncertainty vs. risk
+- from histogram to distribution
+    - commute to work example
+- distributions (briefly explained)
+    - normal distribution
+    - uniform distribution
+    - Poisson distribution
+- logarithms
+- lognormal distribution
+- explain distributions
+    - mean (average)
+    - standard deviation/variance
+    - median
+- calculate with distributions
+    - $E[ax] = aE[x]$
+    - $var(ax) = a^2 var(x)$
+    - Steinerscher Verschiebungssatz
+- distributions (detailed with mean/variance)
+    - show true distribution, and pick 10/100 random data
+- correlation
+    - correlation vs causation: show how a third data point can drive the first two correlations
+- statistical difference/significance
+    - show how two data dots come from the same distribution and can be different
+    - t-test 
