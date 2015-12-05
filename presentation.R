3a70a005e79784a7c229750191528689cc367304
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-12-05 20:27:21 +0100

    first presentation framework

---
+++ presentation.R
@@ -0,0 +1,19 @@
+# require(devtools)
+# library(httr)
+# set_config(use_proxy(url="10.26.0.16", port=3128))
+# install_github("ramnathv/slidify")
+# install_github("ramnathv/slidifyLibraries")
+
+library(slidify)
+
+wd <- "D:/github/statistics101"
+setwd(wd)
+
+author("statistics101")
+slidify("index.Rmd")
+
+# The YAML parsing is done by the yaml package which will treat true as TRUE. 
+# The easiest thing to make this work on the javascript side is to use 
+# page.foo === "TRUE", or even better write a function that will return true 
+# when the input is TRUE.
+# https://github.com/ramnathv/slidify/issues/455#issuecomment-140250538
